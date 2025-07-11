3540598cfd72c3cbfb7e42c2379dbcffbb33454f
John Keller <jkeller2@andrew.cmu.edu>
2025-07-11 00:33:31 +0000

    gimbal now successfully follows body of drome

--- src/gremsy_align_node.cpp
+++ src/gremsy_align_node.cpp
@@ -23,8 +23,10 @@ public:
   GimbalFollowerNode() : Node("gimbal_follower_node")
   {
     publisher_ = this->create_publisher<std_msgs::msg::String>("gimbal_status", 10);
+
     timer_ = this->create_wall_timer(
       1s, std::bind(&GimbalFollowerNode::timer_callback, this));
+
     subscription_ = this->create_subscription<geometry_msgs::msg::Vector3>(
       "move_gimbal_angle", 10,
       std::bind(&GimbalFollowerNode::move_gimbal_callback, this, std::placeholders::_1));
@@ -33,36 +35,25 @@ public:
     my_payload->sdkInitConnection();
     my_payload->checkPayloadConnection();
 
-    // Try different approaches to disable stabilization:
-    
-    // Approach 1: Try different numeric values for the mode
-    // Common gimbal modes: 0=manual, 1=stabilize, 2=lock, 3=follow
-    try {
-        my_payload->setPayloadCameraParam(
-          PAYLOAD_CAMERA_RC_MODE,
-          2,  // Try 2 for "lock" mode
-          PARAM_TYPE_UINT32);
-        RCLCPP_INFO(this->get_logger(), "Set gimbal to lock mode (value 2)");
-    } catch (...) {
-        RCLCPP_WARN(this->get_logger(), "Failed to set lock mode");
-    }
+    // 🎯 SET GIMBAL TO FOLLOW MODE - this makes it move with drone body
+    my_payload->setPayloadCameraParam(
+        PAYLOAD_CAMERA_GIMBAL_MODE,           // Parameter name "GB_MODE"
+        PAYLOAD_CAMERA_GIMBAL_MODE_FOLLOW,    // Value = 2 (follow vehicle)
+        PARAM_TYPE_UINT32);
 
-    // Approach 2: Try to disable stabilization directly
-    // Look for other parameter types that might control stabilization
-    
-    RCLCPP_INFO(this->get_logger(), "Gimbal configured for lock/follow mode");
+    RCLCPP_INFO(this->get_logger(), "Gimbal set to FOLLOW mode - will move with drone body");
   }
 
 private:
   void timer_callback()
   {
     auto message = std_msgs::msg::String();
-    message.data = "Gimbal in LOCK mode";
+    message.data = "Gimbal in FOLLOW mode";
     RCLCPP_INFO(this->get_logger(), "Status: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
 
-  void move_gimbal_callback(const geometry_msgs::msg::Vector3 & msg) const
+  void move_gimbal_callback(const geometry_msgs::msg::Vector3 & msg)
   {
     RCLCPP_INFO(this->get_logger(), "Move Gimbal Request: x=%f y=%f z=%f", msg.x, msg.y, msg.z);
     my_payload->setGimbalSpeed(msg.x, msg.y, msg.z, INPUT_ANGLE);
